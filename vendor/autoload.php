fc6bdc995a2562fe6621c72f29e2e81ae5d03b7c
eZyloT <florian.schoeffl@gmail.com>
2014-12-07 15:00:26 +0100

    :arrow_up: Upgraded composer dependencies

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitd72e959eb07fd6fbf438a6b6eaba0528::getLoader();
+return ComposerAutoloaderInit0a614cff77ead5a55b4cae8c3ae63f1f::getLoader();
