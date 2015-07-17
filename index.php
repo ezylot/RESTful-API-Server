1fc17c8e26169ac0efc93d68690a6217ed538774
ezylot <florian.schoeffl@gmail.com>
2015-07-17 20:30:43 +0000

    Index fild does now redirect to the visual page

--- index.php
+++ index.php
@@ -1,7 +1,2 @@
-<?php
-header("Access-Control-Allow-Orgin: *");
-header("Access-Control-Allow-Methods: *");
-header("Content-Type: application/json");
-
-
-echo json_encode(array("status" => "Failure", "data" => "Please access the site with http://".$_SERVER['HTTP_HOST']."/api/<api-key>/<model>/<arguments>"));
+<? 
+header("Location: app/index.php");
