b6a749928c5a79eea68e19417678d36d1b7429f9
ezylot <florian.schoeffl@gmail.com>
2015-07-17 20:29:53 +0000

    Unit tests do now have the rifht syntax

--- tests/api/model/RandomTest.php
+++ tests/api/model/RandomTest.php
@@ -4,10 +4,8 @@ namespace tests\api\model;
 class randomTest extends \PHPUnit_Framework_TestCase {
     public function testRandomReturnsInt() {
         $w = new \model\random(array(1, 1));
-        $this->assertEquals(1, $w->get(array()));
 
-
-        $w = new \model\random(array(2, 1));
-        $this->assertEquals(2, $w->get(array()));
+        $this->assertEquals(1, $w->get(array(1,1)));
+        $this->assertEquals(2, $w->get(array(2,1)));
     }
 }
--- tests/api/model/apikeyTest.php
+++ tests/api/model/apikeyTest.php
@@ -4,6 +4,6 @@ namespace tests\api\model;
 class apikeyTest extends \PHPUnit_Framework_TestCase {
   public function testKeylengthIsEighteen() {
     $w = new \model\apikey(array());
-    $this->assertEquals(18, strlen($w->get()));
+    $this->assertEquals(18, strlen($w->index()));
   }
 }
